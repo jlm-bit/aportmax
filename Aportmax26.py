3a535a657370fce3823e54fe585ba0e58c5e18aa
jlm-bit <joseplluis.masferrer@gmail.com>
2026-04-12 14:50:25 +0200

    Update Aportmax26.py

--- Aportmax26.py
+++ Aportmax26.py
@@ -483,9 +483,7 @@ with st.expander("ℹ️ Te recomiendo como lograr que tu ahorro sea máximo y d
                     <p style="text-transform: uppercase; letter-spacing: 1px; font-size: 0.7rem; color: #64748b; font-weight: 700; margin-bottom: 15px;">
                         Estrategia Sugerida    {"🔼 (<b> Incrementar</b>" if diferencia_mensual > 0 else "🔽 <b>reducir</b>"} la cuota actual en <b>{abs(diferencia_mensual):,.2f} €</b> durante {meses_restantes} meses).
                     </p>
-                  #  <p style="font-size: 0.95rem; color: #1e293b; line-height: 1.5; margin-bottom: 20px;">
-                  #      Para ajustarte al límite de <b>{max_p:,.2f} €</b>, la nueva cuota mensual recomendada es:
-                  #  </p>
+               
                     <div style="margin-bottom: 10px;">
                         <span style="font-size: 1.4rem; font-weight: 350; color: {color_acentuado};">{nueva_cuota_total:,.2f} €</span>
                         <span style="font-size: 1rem; color: #94a3b8;"> / mes (durante los meses que quedan del año)</span>
@@ -497,13 +495,11 @@ with st.expander("ℹ️ Te recomiendo como lograr que tu ahorro sea máximo y d
             st.markdown(f"""
                 <div style="background-color: #ffffff; border: 1px solid #e2e8f0; padding: 25px; border-radius: 12px; margin: 15px 0;">
                     <p style="text-transform: uppercase; letter-spacing: 1px; font-size: 0.7rem; color: #64748b; font-weight: 700; margin-bottom: 15px;">
-                        Estrategia Sugerida 🔼 Combinar aportaciones extraordinaria con aportaciones periodicas.
+                        Estrategia Sugerida    {"🔼 (<b> Incrementar</b>" if diferencia_mensual > 0 else "🔽 <b>reducir</b>"} la cuota actual en <b>{abs(diferencia_mensual):,.2f} €</b> durante {meses_restantes} meses).
                     </p>
-                    <p style="font-size: 0.95rem; color: #1e293b; line-height: 1.5; margin-bottom: 20px;">
-                        Para ajustarte al límite de <b>{max_p:,.2f} €</b>, la nueva cuota mensual recomendada es:
-                    </p>
+               
                     <div style="margin-bottom: 10px;">
-                        <span style="font-size: 1.4rem; font-weight: 300; color: {color_acentuado};">{nueva_cuota_total:,.2f} €</span>
+                        <span style="font-size: 1.4rem; font-weight: 350; color: {color_acentuado};">{nueva_cuota_total:,.2f} €</span>
                         <span style="font-size: 1rem; color: #94a3b8;"> / mes (durante los meses que quedan del año)</span>
                     </div>
                                                  
