eb56da79a958e2aca48a3e83ba3cffab595085a6
jlm-bit <joseplluis.masferrer@gmail.com>
2026-04-17 23:45:56 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- Aportmax26.py
+++ Aportmax26.py
@@ -178,9 +178,9 @@ def generar_informe_integral_2026(datos):
 # --- 4. SIDEBAR (CON MIN_VALUE=0.0) ---
 
 with st.sidebar:
-    st.header("⚙️ DATOS NECESARIOS",expanded=True)
+    st.header("⚙️ DATOS NECESARIOS")
     
-    with st.expander("👤 DATOS EMPRESA", expanded=True):
+    with st.expander("👤 DATOS EMPRESA",expanded=True):
         sb = st.number_input("Sueldo Bruto Anual (€)", value=0.0, step=1000.0, min_value=0.0)
         e_ahorro = st.number_input("Aportación Mensual Empresa (€)", value=0.0, step=25.0, min_value=0.0)
         e_riesgo = st.number_input("Prima Anual Riesgo PPE (€)", value=0.0, step=25.0, min_value=0.0)
