7b09ec96274b69c565ae33e15f39fa2543ad7a4f
jlm-bit <joseplluis.masferrer@gmail.com>
2026-04-18 15:15:02 +0200

    Change greeting from 'Hello World' to 'Goodbye World'

--- Aportmax26.py
+++ Aportmax26.py
@@ -382,7 +382,8 @@ with tab1:
     cumplimiento_val = float(cumplimiento_plan) if cumplimiento_plan else 0.0
 
     if cumplimiento_val > 100.0:
-        st.warning("⚠️ **Revisa tus datos:** El nivel de aportación calculado supera el límite permitido (100%). Por favor, verifica los importes en "Configuración" y/o revisa "Tu Plan de Accion" ajustando cuotas futuras o suspendiendolas.")
+      
+        st.warning("""⚠️ **Revisa tus datos:** El nivel de aportación calculado supera el límite permitido (100%). Por favor, verifica los importes en Configuración y/o revisa Tu Plan de Accion ajustando cuotas futuras o suspendiendolas.""")
     
     html_card = f"""
     <div style="display: block; text-align: left; margin-top: 10px; width: 100%; background: #f8fafc; padding: 25px; border-radius: 12px; border: 1px solid #e2e8f0; box-shadow: 0 4px 6px -1px rgba(0,0,0,0.05);">
