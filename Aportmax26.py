c9f8b93194944a1b6c731ade46eacdbc9a8d5205
jlm-bit <joseplluis.masferrer@gmail.com>
2026-04-28 11:16:04 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- Aportmax26.py
+++ Aportmax26.py
@@ -401,7 +401,7 @@ with tab1:
     
   
     # --- A PARTIR DE AQUÍ SE EJECUTA TODO LO DEMÁS ---
-    st.success("✅ Cálculos actualizados correctamente")
+    # st.success("✅ Cálculos actualizados correctamente")
     # with st.expander("📝 TUS DATOS para el cálculo (Click para desplegar)", expanded=False):
        
 
