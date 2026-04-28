ea32627b2a32ca34d533f90b7ea7c72c6318485b
jlm-bit <joseplluis.masferrer@gmail.com>
2026-04-28 21:40:04 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- Aportmax26.py
+++ Aportmax26.py
@@ -262,7 +262,7 @@ if 'validado' not in st.session_state:
 
 # --- 2. CONTENEDOR DE ENTRADA PRO ---
 # El expander se cierra (False) cuando ya hemos validado los datos
-with st.expander("📝 CONFIGURACIÓN DE TUS DATOS", expanded=not st.session_state.validado):
+with st.expander("📝 TUS DATOS", expanded=not st.session_state.validado):
     
     col_emp, col_pers = st.columns(2, gap="large")
     
@@ -323,7 +323,7 @@ with st.expander("📝 CONFIGURACIÓN DE TUS DATOS", expanded=not st.session_sta
     
     # BOTÓN DE VALIDACIÓN: Al pulsarlo, el expander se cierra y el programa sigue
     st.write("---")
-    if st.button(" Quiero saber la aportacion adicional máxima posible que puedo aportar a mi PPE", use_container_width=True, type="primary"):
+    if st.button(" Clica aqui para saber la aportacion adicional máxima posible que puedes aportar a tu PPE", use_container_width=True, type="primary"):
         if sb <= 0:
             st.warning("⚠️ Introduce tu Sueldo Bruto para comenzar.")
         else:
