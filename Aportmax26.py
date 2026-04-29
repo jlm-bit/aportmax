38e1e0787140bcf302292ecc90b8460304787541
jlm-bit <joseplluis.masferrer@gmail.com>
2026-04-29 13:30:19 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- Aportmax26.py
+++ Aportmax26.py
@@ -308,6 +308,7 @@ with st.expander("📝 TUS DATOS", expanded=not st.session_state.validado):
         c_m = st.number_input(
             "Tu Aportación Mensual (€)", 
             min_value=0.0, 
+            max_value=5250/12, 
             step=50.0, 
             key="mensual_unique"
         )
@@ -316,6 +317,7 @@ with st.expander("📝 TUS DATOS", expanded=not st.session_state.validado):
         e_y = st.number_input(
             "Aportación Extra ya realizada (€)", 
             min_value=0.0, 
+            max_value=5250, 
             max_value=limite_input if limite_input > 0 else 0.01, 
             step=50.0, 
             key="extra_unique"
@@ -379,14 +381,14 @@ with tab1:
     if st.button("✏️ Modificar datos"):
         st.session_state.validado = False
         st.rerun()
-    with st.expander("🔍 "):
+    with st.expander("🔍 Ver Detalle "):
         st.markdown(f"""
             <div style="font-size: 0.9rem; color: #475569; line-height: 1.6;">
-                El importe de <b>{aportacion_extraordinaria_neta:,.2f} €</b> representa el margen de mayor aportación a tu PPE que aún puedes aprovechar este año. Se calcula restando tus compromisos actuales de tu límite máximo permitido:
+                El importe de <b>{aportacion_extraordinaria_neta:,.2f} €</b> representa la aportación adicional a tu PPE que aún puedes aprovechar este año. Se calcula restando tus aportaciones realizadas y planificadas a tu límite máximo permitido:
                 <ul style="margin-top: 10px;">
-                    <li><b>Límite máximo personal:</b> {max_p:,.2f} €</b>. Considerando tu salario y las contribuciones estimadas de la empresa a tu PPE ({emp_t:,.2f} €).</li>
-                    <li><b>Aportaciones ya realizadas:</b> Descontamos los {ya_aportado:,.2f} € que ya has aportado.</li>
-                    <li><b>Planificación mensual:</b> También restamos los {total_mensual_previsto:,.2f} € que tienes previsto aportar mediante deducción en nómina o aportaciones periódicas hasta diciembre.</li>
+                    <li><b>Límite máximo personal:</b> {max_p:,.2f} €</b>. Considerando las contribuciones estimadas de la empresa a tu PPE ({emp_t:,.2f} €).</li>
+                    <li><b>Aportaciones ya realizadas:</b>  {ya_aportado:,.2f} € ya aportados hasta ahora.</li>
+                    <li><b>Aportaciones planificación futuras:</b>  {total_mensual_previsto:,.2f} € que tienes previsto aportar mediante deducción en nómina o aportaciones periódicas hasta diciembre.</li>
                     <li><b>Objetivo de ahorro:</b> Con la cifra señadada de {aportacion_extraordinaria_neta:,.0f} €, alcanzarás un total de <b>{total_aportado_anual:,.2f} €</b>, cubriendo el <b>{porcentaje_cumplimiento:.1f}%</b> de tu capacidad de aportación anual permitida.</li>
                     <li><b>Fiscalidad:</b> Despliega la seccion de ℹ️ Ahorro Fiscal.</li>
                     <li><b>Acción:</b> Despliega la seccion de ℹ️ Tu Plan de Acción: te ayudaremos a conseguir tu objetivo.</li>
